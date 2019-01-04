0a201cc7b57d42676bd75337bf2fe41c6d0b4bd1
Daverson <dds.saldanha@gmail.com>
2019-01-04 17:52:47 -0200

    11.1. Criando diretivas customizadas

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { CampoColoridoDirective } from './campo-colorido.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FuncionarioCardComponent,
-    FuncionarioFormComponent
+    FuncionarioFormComponent,
+    CampoColoridoDirective
   ],
   imports: [
     BrowserModule,
