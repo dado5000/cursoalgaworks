16009e889537986aa28da4bc6ac4bd3cec577ef2
Daverson <dds.saldanha@gmail.com>
2019-02-06 11:04:20 -0200

    Aula 13.7 Mascara no Input

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import { CalendarModule } from 'primeng/components/calendar/calendar';
-import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
-import {DropdownModule} from 'primeng/components/dropdown/dropdown';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+
 
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
@@ -41,6 +43,7 @@ import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.co
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
+    InputMaskModule,
     CurrencyMaskModule
   ],
   providers: [],
