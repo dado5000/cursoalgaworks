cd1186120b99f9c73d93d3c02a9f43972c714c08
Daverson <dds.saldanha@gmail.com>
2019-02-06 14:35:27 -0200

    Aula 13.8 Validadores e mensagens

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
@@ -35,6 +36,7 @@ import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.co
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     InputTextModule,
     ButtonModule,
     DataTableModule,
