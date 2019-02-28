4e05c3287cc09a6f8432e45a0f9923dc07f0837e
Daverson <dds.saldanha@gmail.com>
2019-02-28 09:03:33 -0300

    Aula 17.6 paginação lasy

--- algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -2,6 +2,8 @@ import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 
 import { Component, OnInit } from '@angular/core';
 
+import { LazyLoadEvent } from 'primeng/components/common/api';
+
 @Component({
   selector: 'app-lancamentos-pesquisa',
   templateUrl: './lancamentos-pesquisa.component.html',
@@ -9,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent  implements OnInit {
 
+  totalRegistros = 0;
   filtro = new LancamentoFiltro();
   lancamentos = [];
   pt: any;
@@ -28,15 +31,23 @@ export class LancamentosPesquisaComponent  implements OnInit {
       today: 'Hoje',
       clear: 'limpar'
   }
-
-    this.pesquisar();
+    /* Descontinuado na aula 17.6 */
+    // this.pesquisar();
   }
 
-  pesquisar() {
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
     this.lancamentoService.pesquisar( this.filtro )
       .then( resultado => {
+        this.totalRegistros = resultado.total;
         this.lancamentos = resultado.lancamentos;
       } );
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
   }
 
 }
