26525051b3d33d140f04bf73eb3e8e55788c9d18
Daverson <dds.saldanha@gmail.com>
2019-03-13 11:22:25 -0300

    Backend, Ajustes no projeto e implementações

--- fullstack/src/main/resources/db/migration/V02__criar_e_registrar_pessoa.sql
+++ fullstack/src/main/resources/db/migration/V02__criar_e_registrar_pessoa.sql
@@ -12,12 +12,12 @@ CREATE TABLE pessoa (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado)
-	 values ('Daverson Dantas Saldanha',"1","rua Jose Augusto Mengue","324",null,"Pq dos Ypes","13160-000","Artur Nogueira","São Paulo");
+	 values ('Daverson Dantas Saldanha','1','rua Jose Augusto Mengue','324',null,'Pq dos Ypes','13160-000','Artur Nogueira','SP');
 INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado)
-	 values ('Lidiane Ap Saldanha',"1","rua Jose Augusto Mengue","324",null,"Pq dos Ypes","13160-000","Artur Nogueira","São Paulo");
+	 values ('Lidiane Ap Saldanha','1','rua Jose Augusto Mengue','324',null,'Pq dos Ypes','13160-000','Artur Nogueira','SP');
 INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado)
-	 values ('Ebenezer Piscinas',"1","rua Nicolau de Sá","348","Fabrica","Itamaraty","13160-000","Artur Nogueira","São Paulo");
+	 values ('Ebenezer Piscinas','1','rua Nicolau de Sá','348','Fabrica','Itamaraty','13160-000','Artur Nogueira','SP');
 INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado)
-	 values ('João das Couves',"0","rua das Orquideas","40",null,"Amoreiras","18160-354","Maceio","Alagoas");
+	 values ('João das Couves','0','rua das Orquideas','40',null,'Amoreiras','18160-354','Maceio','AL');
 INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado)
-	 values ('Beatriz Oliveira',"0","Av Gen Ozorio","1459","Praça das Flores","Centro","44789-565","Fortaleza","Ceara");
+	 values ('Beatriz Oliveira','0','Av Gen Ozorio','1459','Praça das Flores','Centro','44789-565','Fortaleza','CE');
