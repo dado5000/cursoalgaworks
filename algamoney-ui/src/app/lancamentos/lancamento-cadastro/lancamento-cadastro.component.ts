08a5b1308522b0b8882136064f732e68a64dab5b
Daverson <dds.saldanha@gmail.com>
2019-04-29 16:49:12 -0300

    Aula 21.11. Criando validações customizadas

--- algamoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,5 +1,5 @@
 import { ToastyService } from 'ng2-toasty';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 import { ErrorHandlerService } from 'app/core/error-handler.service';
@@ -86,7 +86,7 @@ export class LancamentoCadastroComponent implements OnInit  {
       tipo: [ 'RECEITA', Validators.required ],
       dataVencimento: [ null, Validators.required ],
       dataPagamento: [],
-      descricao: [null, [ Validators.required, Validators.minLength(5) ]],
+      descricao: [null, [ this.validarObrigatoriedade, this.validarTamanhoMinimo(5) ]],
       valor: [ null, Validators.required ],
       pessoa: this.formBuilder.group({
         codigo: [ null, Validators.required ],
@@ -98,6 +98,16 @@ export class LancamentoCadastroComponent implements OnInit  {
       }),
       observacao: []
     });
+  }
+
+  validarObrigatoriedade(input: FormControl) {
+    return (input.value ? null : { obrigatoriedade: true });
+  }
+
+  validarTamanhoMinimo(valor: number) {
+    return (input: FormControl) => {
+      return (!input.value || input.value.length >= valor) ? null : { tamanhoMinimo: { tamanho: valor } };
+    };
   }
 
   /* Métodos de SERVIÇO  ****************************************************************************/
